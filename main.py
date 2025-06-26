f09437eb0a6ba30d50624ead394ede57da5807e5
Vishu Ahlawat <vishucr2007@gmail.com>
2025-06-26 23:43:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 pygame.init()
 
 # Define the window dimensions
-window_width = 800
-window_height = 400
+window_width = 1920
+window_height = 1880
 
 # Create the game window
 window = pygame.display.set_mode((window_width, window_height))
@@ -33,13 +33,13 @@ while True:
     pygame.display.update()
 
     keys= pygame.key.get_pressed()
-    if keys[K_w] and paddle1.y>0:
-        paddle1.y+=5
+    if keys[K_w] and paddle1.y > 0:
+        paddle1.y-=5
     if keys[K_s] and paddle1.y < window_height - paddleh:
-        paddle1.y -= 5
+        paddle1.y += 5
     if keys[K_UP] and paddle2.y > 0:
         paddle2.y -= 5
-    if keys[K_DOWN] and paddle2.y < window_height - paddlehe:
+    if keys[K_DOWN] and paddle2.y < window_height - paddleh:
         paddle2.y += 5
     ball.x += ball_speed_x
     ball.y += ball_speed_y
